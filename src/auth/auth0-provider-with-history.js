9cdc66a690de5e647fbee2cc3f45bb2eb9712ddd
Abhay5855 <ap3919529@gmail.com>
2022-01-10 10:10:09 +0530

    passing the domain , client id , redirect uri to the Auth0 component to redirect back to the source

--- src/auth/auth0-provider-with-history.js
+++ src/auth/auth0-provider-with-history.js
@@ -1,17 +1,27 @@
+import { useHistory, useSearchParams } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-import {useHistory} from 'react-router-dom';
+// Get access to domain and id
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-const Auth0ProviderWithHistory  = () => {
+const history = useHistory();
 
-       return (
-           <>
-
-           </>
-       )
-}
-
-
+const Auth0ProviderWithHistory = ({ children }) => {
+  return (
+    <>
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        redirectUri={window.location.origin}
+        onRedirectCallback={onRedirectCallback}
+      >
+        {children}
+      </Auth0Provider>
+    </>
+  );
+};
 
 export default Auth0ProviderWithHistory;
