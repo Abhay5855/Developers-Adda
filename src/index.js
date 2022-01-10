59bef43d3ff9f7d5ac19aec06fc4818dd5ba0150
Abhay5855 <ap3919529@gmail.com>
2022-01-10 12:00:36 +0530

    fix : remove the auth0 authentication from the app

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Auth0ProviderWithHistory>
         <App />
       </Auth0ProviderWithHistory>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
