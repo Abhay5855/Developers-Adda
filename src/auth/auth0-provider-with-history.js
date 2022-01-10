95d888dcb256f723ee033076ad1190619aab5e55
Abhay5855 <ap3919529@gmail.com>
2022-01-10 11:19:58 +0530

    Providing the main App with the AuthProvider and wrapping the App

--- src/auth/auth0-provider-with-history.js
+++ src/auth/auth0-provider-with-history.js
@@ -1,15 +1,26 @@
-import { useHistory, useSearchParams } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
 
-// Get access to domain and id
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useHistory} from "react-router-dom";
+
+
+
+
+const Auth0ProviderWithHistory = ({ children }) => {
+
+
+    // Get access to domain and id
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-const history = useHistory();
+let history = useHistory();
 
-const Auth0ProviderWithHistory = ({ children }) => {
+
+// Return to the appState or return to the location
+const onRedirectCallback = (appState) => {
+       history.push(appState?.returnTo || window.location.pathname);
+}
   return (
     <>
       <Auth0Provider
